875aaecf4d1f81d72d6181b35c7a3f4661050a32
Yogesh Seetharaman <yogeshs@umich.edu>
2014-11-14 15:31:57 -0500

    trying things out

--- src/main/java/eecs285/App.java
+++ src/main/java/eecs285/App.java
@@ -10,8 +10,15 @@ public class App
 {
     public static void main( String[] args )
     {
-      JumblrClient client = new JumblrClient("consumer_key", "consumer_secret");
-      client.setToken("oauth_token", "oauth_token_secret");
+      // Authenticate via OAuth
+      JumblrClient client = new JumblrClient(
+              "fP4T709QbfTBOaR4mHLNFeE6BgbwxQw10qSypEN9onlWKGwsBn",
+              "bnOedxoYrDveIr6mbg793VLkyhEF8RviaYyFFvuiWpajXZCFsB"
+      );
+      client.setToken(
+              "UmFBwJCEgshbDAwKwHbRyfHwxo7k2VO4OI41IOnv1Ouklk3XnE",
+              "zpDQUGI0z81X3KeUiDgTLvAeZ6ETBhjepVQsK1hnOJSq6occAz"
+      );
 
       System.out.println( "Hello World!" );
     }
