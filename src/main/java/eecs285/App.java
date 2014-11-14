a407fdcad2500c746e377ac5d40b4832e5604f18
Yogesh Seetharaman <yogeshs@umich.edu>
2014-11-14 15:40:06 -0500

    trying more things

--- src/main/java/eecs285/App.java
+++ src/main/java/eecs285/App.java
@@ -1,6 +1,7 @@
 package eecs285;
 
 import com.tumblr.jumblr.JumblrClient;
+import com.tumblr.jumblr.types.Blog;
 
 /**
  * Hello world!
@@ -15,11 +16,19 @@ public class App
               "fP4T709QbfTBOaR4mHLNFeE6BgbwxQw10qSypEN9onlWKGwsBn",
               "bnOedxoYrDveIr6mbg793VLkyhEF8RviaYyFFvuiWpajXZCFsB"
       );
+      System.out.println( "heheh" );
+
       client.setToken(
               "UmFBwJCEgshbDAwKwHbRyfHwxo7k2VO4OI41IOnv1Ouklk3XnE",
               "zpDQUGI0z81X3KeUiDgTLvAeZ6ETBhjepVQsK1hnOJSq6occAz"
       );
 
-      System.out.println( "Hello World!" );
+
+      // Make the request
+      Blog blog = client.blogInfo("delicatedragonstarlight.tumblr.com");
+
+      System.out.println( blog.getName() );
+
+      System.out.println( "heheh" );
     }
 }
