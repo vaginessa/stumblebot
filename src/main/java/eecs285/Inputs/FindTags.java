04a7fe68e6ea6895e253782d3b7eb0d32fda44ac
amesjm <amesjm@gmail.com>
2014-11-17 14:48:20 -0500

    Worked on FindTags more.  Sorted output Map object.

--- src/main/java/eecs285/Inputs/FindTags.java
+++ src/main/java/eecs285/Inputs/FindTags.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.PriorityQueue;
+import java.util.TreeMap;
 import java.util.Vector;
 
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
@@ -28,6 +29,8 @@ public class FindTags
   static List<String> findFrom(List<Post> inPosts)
   {
     Map<String, Integer> tagMap = new HashMap<String, Integer>();
+    ValueComparator bvc =  new ValueComparator(tagMap);
+    TreeMap<String,Integer> sorted_tagMap = new TreeMap<String,Integer>(bvc);
     for(Post post : inPosts)
     {
       for(String tag : post.getTags())
@@ -36,16 +39,37 @@ public class FindTags
         tagMap.put(tag, val == null ? 1 : val + 1);
       }
     }
-    Comparator<Entry<String, Integer>> pQueuecomp = new pQueueComparator();
+    /*Comparator<Entry<String, Integer>> pQueuecomp = new pQueueComparator();
     PriorityQueue<Entry<String, Integer>> pQueue = new PriorityQueue<Entry<String, Integer>>(tagMap.size(), pQueuecomp);
     for(Entry<String, Integer> entry : tagMap.entrySet())
     {
       pQueue.add(entry);
     }
-    List<String> topTags = new Vector<String>();
-    //topTags.addAll((1..maxTags).collect{pQueue.poll();};
+    List<String> topTags = new Vector<String>();*/
+    sorted_tagMap.putAll(tagMap);
+    for(int tagNum = 0; tagNum < maxTags; ++tagNum)
+    {
+      topTags.add(tagNum, pQueue.poll().getKey());
+    }
     return topTags;
   }
 }
 
+//http://stackoverflow.com/questions/109383/how-to-sort-a-mapkey-value-on-the-values-in-java
+class ValueComparator implements Comparator<String> {
+
+  Map<String, Integer> base;
+  public ValueComparator(Map<String, Integer> base) {
+      this.base = base;
+  }
+
+  // Note: this comparator imposes orderings that are inconsistent with equals.    
+  public int compare(String a, String b) {
+      if (base.get(a) >= base.get(b)) {
+          return -1;
+      } else {
+          return 1;
+      }
+  }
+}
 
