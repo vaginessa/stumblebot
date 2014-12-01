381ddb619ae99e2f6d35bf3b1c2652f9ccba9c41
jyh947 <jyh947@gmail.com>
2014-11-30 22:33:56 -0500

    Get rid of warnings

--- src/main/java/eecs285/GUI/Events/FetchTagsAction.java
+++ src/main/java/eecs285/GUI/Events/FetchTagsAction.java
@@ -2,10 +2,7 @@ package eecs285.GUI.Events;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 import java.util.List;
-
-import com.tumblr.jumblr.types.Post;
 
 import eecs285.App;
 import eecs285.GUI.Simulator;
@@ -18,9 +15,10 @@ public class FetchTagsAction implements ActionListener
   {
     List<String> retrievedTags = FindTags.findFrom(App.globalPosts);
     App.globalTagsFound.clear();
-    for(String stringIterCurrent : retrievedTags )
+    for( String stringIterCurrent : retrievedTags )
     {
-      if( !App.globalTagsFound.contains(stringIterCurrent) && !App.globalTagsSeeded.contains(stringIterCurrent))
+      if( !App.globalTagsFound.contains(stringIterCurrent)
+          && !App.globalTagsSeeded.contains(stringIterCurrent) )
       {
         App.globalTagsFound.add(stringIterCurrent);
         System.out.println(stringIterCurrent);
--- src/main/java/eecs285/Reblog/Filter.java
+++ src/main/java/eecs285/Reblog/Filter.java
@@ -19,8 +19,8 @@ class PostBlogLikesComparator implements Comparator<Post>
 {
   public int compare(Post a, Post b)
   {
-    if ( App.client.blogInfo(a.getBlogName()).getPostCount()
-            >= App.client.blogInfo(b.getBlogName()).getPostCount())
+    if( App.client.blogInfo(a.getBlogName()).getPostCount() >= App.client
+        .blogInfo(b.getBlogName()).getPostCount() )
       return -1;
     else
       return 1;
@@ -32,94 +32,99 @@ public class Filter
 {
   static int numPostsToReblog = 10;
 
-  static Map<String, Map<String, Integer> > tagMap = new HashMap<>();
+  static Map<String, Map<String, Integer>> tagMap = new HashMap<>();
   static TreeSet<Post> postTreeSet;
   static HashSet<Long> alreadyPosted = new HashSet<>();
 
   // gets top n posts to reblog by checking poster's popularity
-  public static List<Post> postsToReblog(List<Post> inPosts) {
+  public static List<Post> postsToReblog(List<Post> inPosts)
+  {
     postTreeSet = new TreeSet<>(new PostBlogLikesComparator());
     postTreeSet.addAll(inPosts);
 
     List<Post> topNPosts = new ArrayList<>();
 
     Iterator<Post> it = postTreeSet.iterator();
-    while (topNPosts.size() < numPostsToReblog) {
+    while( topNPosts.size() < numPostsToReblog )
+    {
       Post p = it.next();
       long id = p.getId();
-      if (!alreadyPosted.contains(id)) {
+      if( !alreadyPosted.contains(id) )
+      {
         alreadyPosted.add(id);
         topNPosts.add(p);
       }
     }
     /*
-    for (int i = 0; i < numPostsToReblog; ++i)
-      topNPosts.add(it.next());
-    */
+     * for (int i = 0; i < numPostsToReblog; ++i) topNPosts.add(it.next());
+     */
 
     return topNPosts;
   }
 
-  
+
   /*
-  Each Post is a list of tags
-  List of of List of tags
- 
-  Get a post, find another tag that post has been seen with
- 
-  Input List of Lists, shoot out Hashmap <- Probably different functions
-  Goal is to build the Hashmap, you put in a post,
-  in fires back associated tags;
-  add count if things occur multiple times;
- 
- 
-  * HashMap<String, HashMap<String, int> > tagMap
-  * First HashMap maps tag (aka a post title) to another Hashmap;
-  *  Second Hashmap maps Strings to ints.
-  *  
-  *  You can iterate through maps, so use that to find top n tags, assemble
-  *  into list
-  *  and push out
-  */
+   * Each Post is a list of tags List of of List of tags
+   * 
+   * Get a post, find another tag that post has been seen with
+   * 
+   * Input List of Lists, shoot out Hashmap <- Probably different functions Goal
+   * is to build the Hashmap, you put in a post, in fires back associated tags;
+   * add count if things occur multiple times;
+   * 
+   * 
+   * HashMap<String, HashMap<String, int> > tagMap First HashMap maps tag (aka a
+   * post title) to another Hashmap; Second Hashmap maps Strings to ints.
+   * 
+   * You can iterate through maps, so use that to find top n tags, assemble into
+   * list and push out
+   */
   static List<String> tagsFromPost(Post inPost)
   {
     List<String> postTags = inPost.getTags();
     HashSet<String> returnTags = new HashSet<>();
     int count = 0;
 
-    for (String tag : postTags) {
-      if (!tagMap.containsKey(tag)) {
+    for( String tag : postTags )
+    {
+      if( !tagMap.containsKey(tag) )
+      {
         tagMap.put(tag, new HashMap<String, Integer>());
       }
 
-      for (String tag2 : postTags) {
-        if (!tagMap.get(tag).containsKey(tag2)) {
+      for( String tag2 : postTags )
+      {
+        if( !tagMap.get(tag).containsKey(tag2) )
+        {
           tagMap.get(tag).put(tag2, 1);
         }
-        else {
+        else
+        {
           count = tagMap.get(tag).get(tag2);
           tagMap.get(tag).put(tag2, count + 1);
         }
       }
     }
 
-    for (String tag : postTags) {
-      for (String tag2 : tagMap.get(tag).keySet()) {
+    for( String tag : postTags )
+    {
+      for( String tag2 : tagMap.get(tag).keySet() )
+      {
         returnTags.add(tag2);
       }
-      if (returnTags.size() > 20)
+      if( returnTags.size() > 20 )
         break;
     }
     List<String> tagsStrings = new ArrayList<String>(returnTags);
 
     System.out.println("Tags for post: " + inPost);
-    for (String tag : tagsStrings)
+    for( String tag : tagsStrings )
       System.out.println(tag);
     System.out.println();
 
     return tagsStrings;
   }
-  
+
   static String titleForPost(Post inPost)
   {
     return inPost.getBlogName() + " Repost";
@@ -127,6 +132,6 @@ public class Filter
 
   static void getTextForPost(Post inPost)
   {
-    List<String> tags = inPost.getTags();
+    //List<String> tags = inPost.getTags();
   }
 }
