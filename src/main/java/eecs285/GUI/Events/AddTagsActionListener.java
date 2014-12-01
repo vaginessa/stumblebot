c9b1e62cf2d5f22d910e8627c5291e70adef2c7f
jyh947 <jyh947@gmail.com>
2014-12-01 13:15:57 -0500

    Uncaps

--- src/main/java/eecs285/GUI/Events/AddTagsActionListener.java
+++ src/main/java/eecs285/GUI/Events/AddTagsActionListener.java
@@ -15,7 +15,7 @@ public class AddTagsActionListener implements ActionListener
         "Are you sure you want to add these tag(s)?", "Confirmation",
         JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION )
     {
-      String userInput = AddTagsEvent.getText().replaceAll("\\s+", "");
+      String userInput = AddTagsEvent.getText().toLowerCase().replaceAll("\\s+", "");
       System.out.println(userInput);
       String[] userInputParse = userInput.split(",");
       for( String stringIter : userInputParse )
