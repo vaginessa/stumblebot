918b133815fa5b42fb46aa6ffa80bf28ecef6e3c
Yogesh Seetharaman <yogeshs@umich.edu>
2014-12-01 12:48:49 -0500

    merge conlict again

--- src/main/java/eecs285/GUI/Events/PostButtonAction.java
+++ src/main/java/eecs285/GUI/Events/PostButtonAction.java
@@ -1,9 +1,11 @@
 package eecs285.GUI.Events;
 
+import eecs285.App;
+import eecs285.Reblog.Reblog;
+
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import eecs285.Reblog.Reblog;
 
 /**
  * Created by yogesh on 11/16/14.
@@ -12,13 +14,7 @@ public class PostButtonAction implements ActionListener
 {
   public void actionPerformed(ActionEvent event)
   {
-<<<<<<< HEAD
-    @SuppressWarnings("unused")
-    Reblog action = new Reblog();
-=======
-    if( JOptionPane.showConfirmDialog(App.win,
-        "Are you sure you want to repost all " + App.globalPosts.size()
-            + " post(s)?", "Confirmation", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION )
+    if( JOptionPane.showConfirmDialog(App.win, "Are you sure you want to repost all " + App.globalPosts.size() + " post(s)?", "Confirmation", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION )
     {
       Reblog.ReblogAll();
       JOptionPane.showMessageDialog(App.win,
@@ -29,6 +25,5 @@ public class PostButtonAction implements ActionListener
     {
       return;
     }
->>>>>>> 5c4876c71b763e95b6f68f66c94cff824f66f58b
   }
 }
